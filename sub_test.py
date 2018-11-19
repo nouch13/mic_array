f54c484798425e4656c46772a18db4ec32c65f1d
Shotaro Takenouchi <0tamar0pokemon@gmail.com>
2018-11-19 14:25:54 +0900

    角度と笑いの種類をSubscribe時に同期してみた

--- sub_test.py
+++ sub_test.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from time import sleep
 import paho.mqtt.client as mqtt
 
 topic = '806/sound/#'
+host = 'localhost'
+port = 1883
 
 def on_connect(client, userdata, flags, respons_code):
-    print('watch %s' % topic)
+    print('status {0}'.format(respons_code))
     client.subscribe(topic)
 
 def on_message(client, userdata, msg):
-    # doa_angle = 0.0
-    print(msg.topic + ' ' + str(msg.payload.decode(encoding='utf-8')))
-    if msg.topic == '806/sound/mic_array/doa':
-        doa_angle = float(msg.payload.decode(encoding='utf-8'))
-    if msg.topic == '806/sound/usb_mic/laughter':
-        laughter_cls = msg.payload.decode(encoding='utf-8')
-        print(laughter_cls, doa_angle)
-        # print('笑いの種類:%s  音の方向:%.1f' % (laughter_cls, doa_angle))
+    # print(msg.topic + '' + str(msg.payload))
+    topics = msg.topic.split('/')
+    if topics[-1] == 'doa':
+        store_doa(msg)
+    elif topics[-1] == 'laughter':
+        # print(msg.payload.decode('utf-8'))
+        doa_data = open('./tmp_doa.txt', 'r')
+        angle_doa = float(doa_data.read())
+        print('笑いの種類: %s \t 到来角度: %.1f' % (msg.payload.decode('utf-8'), angle_doa))
 
 
-def main():
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect('localhost', 1883, keepalive=60)
-    client.loop_forever()
+def store_doa(msg):
+    with open('./tmp_doa.txt', 'w') as file:
+        # 角度をファイルに保存
+        file.write(msg.payload.decode('utf-8'))
+
 
 if __name__ == '__main__':
-    main()
+    client = mqtt.Client(protocol=mqtt.MQTTv311)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(host, port=port, keepalive=60)
+
+    client.loop_forever()
